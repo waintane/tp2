933b061822da3abb5ee05db642afaf866d47bd53
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-05-11 19:14:20 -0400

    sm15 modification du css général

--- single-post.php
+++ single-post.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <main class="site__main">
-     <h1>-------  single-post.php ----------</h1>
+     <h1>Description du cours</h1>
      <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
           <article class="cours">
               
