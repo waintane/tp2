6031f9a9cc0283f924eda8009a912729608c2ae6
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-05-19 12:25:42 -0400

    ef-OPT ajout de la base des blocs pour les ateliers

--- front-page.php
+++ front-page.php
@@ -8,7 +8,12 @@
 <h2>Les événements de l'année</h2>
 <?php wp_nav_menu(array(
         "menu"=> "evenement",
-        "container" => "nav")); ?>        
+        "container" => "nav")); ?>  
+              
+<h2>Les atelierss à venir</h2>
+<?php wp_nav_menu(array(
+        "menu"=> "atelier",
+        "container" => "nav")); ?>   
 
    <?php if (have_posts()): the_post(); ?>
         <h2><?php the_title() ?></h2>
